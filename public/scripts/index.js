8389254b620d32cb9d1237a8ef28b79b74bb70ef
Liumingxun <18945504808@163.com>
2022-04-15 18:01:55 +0800

    feat: build socket.io server

---
+++ public/scripts/index.js
@@ -0,0 +1,43 @@
+const socket = io('localhost:5000')
+
+console.log(socket)
+
+function updateUserList(socketList) {
+  const activeUserList = document.getElementById('user-list')
+
+  socketList.forEach(socketId => {
+    const alreadyExist = document.getElementById(socketId)
+    if (!alreadyExist) {
+      const userEl = document.createElement('li')
+      userEl.setAttribute('id', socketId)
+      userEl.textContent = socketId
+      activeUserList.append(userEl)
+    }
+  })
+}
+
+socket.on("remove-user", ({ socketId }) => {
+  const elToRemove = document.getElementById(socketId);
+
+  if (elToRemove) {
+    elToRemove.remove();
+  }
+})
+
+
+socket.on('update-user-list', (res) => {
+  const {user:userList} = res
+  console.log(userList)
+  updateUserList(userList)
+})
+
+const devices = navigator.mediaDevices
+console.log(devices)
+devices.getUserMedia({video: true, audio: true})
+  .then((stream) => {
+    const localVideo = document.getElementById('local-video')
+    if (localVideo) localVideo.srcObject = stream
+  })
+  .catch(err => {
+    console.error(err.message)
+  })
--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,7 @@ export class Server {
   private httpServer!: HTTPServer
   private app!: Application
   private io!: SocketIOServer
+  private activeSockets: string[] = []
   private port
 
   constructor(port?: number) {
@@ -24,7 +25,12 @@ export class Server {
   private initialize() {
     this.app = express()
     this.httpServer = createServer(this.app)
-    this.io = new SocketIO(this.httpServer)
+    this.io = new SocketIO(this.httpServer, {
+      cors: {
+        origin: 'http://192.168.31.123:5000',
+        methods: ['GET', 'POST'],
+      },
+    })
 
     this.configureApp()
     this.configureRoutes()
@@ -44,13 +50,33 @@ export class Server {
   }
 
   private handleSocketConnection() {
+    // 当有 socket 连接时
     this.io.on('connection', (socket) => {
-      console.log('socket connection', socket)
+      const exist = this.activeSockets.find(item => item === socket.id)
+      if (!exist) {
+        // 如果是新连接
+        this.activeSockets.push(socket.id)
+        // 给当前新连接更新用户列表
+        socket.emit('update-user-list', {
+          user: this.activeSockets.filter(item => item !== socket.id),
+        })
+        // 给其他连接更新用户列表
+        socket.broadcast.emit('update-user-list', {
+          user: [socket.id],
+        })
+      }
+      console.log(this.activeSockets)
+      socket.on('disconnect', () => {
+        this.activeSockets = this.activeSockets.filter(exist => exist !== socket.id)
+        socket.broadcast.emit('remove-user', {
+          socketId: socket.id,
+        })
+      })
     })
   }
 
   public listen(callback: (port: number) => void): void {
-    this.httpServer.listen(this.port, () => callback(this.port))
+    this.httpServer.listen(this.port, '0.0.0.0', () => callback(this.port))
   }
 
   public getRequestListener() {
